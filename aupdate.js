1cac7c962bb4b0ad6b100b3d4209ca5b4dc36916
Bolt Scraper <krishnarathore.me@gmail.com>
2025-08-10 22:33:33 +0530

    Update aupdate.js

--- aupdate.js
+++ aupdate.js
@@ -3,6 +3,8 @@
   "downloadUrl": "https://boltscraper.com/install-apollo-io-scraper",
   "releaseDate": "2025-07-08",
   "releaseNotes": [
+    "Now Extracts 50+ Data Fields",
+    "1.5x Fast Scraping",
     "Basic bugs fixed"
   ],
   "minVersion": "1.0.0"
